2d17be65880fa47a6b2b7f18f43ac4acd9f8a084
Usama Bin Hafeez Abbasi <usamahafeez.abbasi1234@gmail.com>
2024-11-18 13:20:52 +0500

    Completed 6 tasks for input and output sections

---
+++ Variables & Data Types/Input and Output.py
@@ -0,0 +1,17 @@
+# Write a program to take a user's name as input and greet them with their name.
+
+user_name = input("Enter you name: ")
+print("Welcome",user_name)
+
+# Create a program that asks for the user's birth year and calculates their age.
+
+current_year = 2024
+user_entered_year = input("Enter your birth year: ")
+user_entered_year = int(user_entered_year)
+
+ageUser = current_year - user_entered_year
+print("Your age is: ", ageUser)
+
+
+# How can you take two inputs from the user in a single line? Write code to demonstrate this.
+
